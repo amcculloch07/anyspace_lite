1a171974d23419ba9dd235fac10a9b63e3187aea
Raghu Betina <raghu@firstdraft.com>
2016-11-29 19:59:31 +0000

    Validate presence of coordinators.email_address

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -5,6 +5,8 @@ class Coordinator < ApplicationRecord
 
   # Validations
 
+  validates :email_address, :presence => true
+
   validates :first_name, :presence => true
 
   validates :last_name, :presence => true
